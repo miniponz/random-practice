66a4eb7b31078016999cb17d8efea9258a8f2aa3
Cara Ponzini <ponzini@gmail.com>
2019-01-22 19:56:29 -0800

    sound and color, except no color

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+//MAKE ARRAYS
+        var pronoun = ['He', 'She', 'They']
+        var frequency = ['rarely', 'habitually', 'infrequently', ]
+        var verb = ['jest', 'prod' , 'exclaim' , 'delight']
+        var last = ['with ease.', 'the African Dog.', 'petulantly.', 'your mother.']
+
+
 //FUNCTION
 
     //1. Generate random words.
 
+
+
     //2. display the result, probably inserting into an element by Id.
+
+
+        //get element by id = 'prompt'
 
 
