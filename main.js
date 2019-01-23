45f39122c181e0212ee746c042685b23e57e451c
Cara Ponzini <ponzini@gmail.com>
2019-01-23 11:51:17 -0800

    working model of random caption geenrator

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-//MAKE ARRAYS
-        var pronoun = ['He', 'She', 'They']
-        var frequency = ['rarely', 'habitually', 'infrequently', ]
-        var verb = ['jest', 'prod' , 'exclaim' , 'delight']
-        var last = ['with ease.', 'the African Dog.', 'petulantly.', 'your mother.']
-
-
 //FUNCTION
+function blowMind(){
+
+//MAKE ARRAYS
+        var pronouns = ['He', 'She', 'It']
+        var frequencies = ['rarely', 'habitually', 'infrequently', ]
+        var verbs = ['jests', 'prods' , 'exclaims' , 'delights']
+        var lasts = ['with ease.', 'the African Dog.', 'petulantly.', 'your mother.']
+        
+        
 
     //1. Generate random words.
 
+        var pronoun = pronouns [Math.floor(Math.random() * pronouns.length)];
+        var frequency = frequencies [Math.floor(Math.random() * frequencies.length)];
+        var verb = verbs [Math.floor(Math.random() * verbs.length)];
+        var last = lasts [Math.floor(Math.random() * lasts.length)];
 
-
-    //2. display the result, probably inserting into an element by Id.
-
+        console.log(pronoun);
+        
+        
+        
+        //2. display the result, probably inserting into an element by Id.
+        
+        document.getElementById("prompt").textContent = pronoun + ' '+ frequency +' '+ verb + ' '+ last;
 
         //get element by id = 'prompt'
-
+}
 
